4c9e045f60ce74718ae182fb817bf9bc899180fc
tutorialmulu <77432915+tutorialmulu@users.noreply.github.com>
2021-01-16 20:23:02 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -2,15 +2,20 @@ const help = (prefix) => {
 	return `   *[SAKURA-BOT]*
 ͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏
 ┏━━━━━━━━━━━━━━━━━━━━━┓
-┃ *⚠️ NO SPAM!! NO CALL!!! ⚠️*
-┃ *LANGGAR BLOCK/BANNED!!!*
+●⧐JANGAN SPAM BOT
+●⧐JANGAN CALL.VC BOT
+●⧐LANGGAR GW BAND
+●⧐BAKAL BALES SANS:V
+●⧐NO KECOT!
+●⧐BOT GRATIS!TDK BOLE DI
+PERJUALKAN!!
 ┣━━━━°❀ *❬ ABOUT ❭* ❀°━━━━━━┛
 ┃
 ┃⊱❥ *SAKURA-BOT*
-┃⊱❥ *V 6.0*
+┃⊱❥ *V 6.1*
 ┃⊱❥ *http://wa.me/6281316105119*
 ┃⊱❥ *Achillboyy*             
-┃
+┃⊱❥ *PERHATIKAN TANDA²YG DI BAWAH INI ✅(BISA DIGUNAKAN) ❌(TIDAK BISA DI GUNAKAN) 👀(PERBAIKAN)
 ┣━━°❀ ❬ *TENTANG BOT* ❭ ❀°━━┓
 ┃
 ┣⊱❥ *${prefix}info*
@@ -20,195 +25,194 @@ const help = (prefix) => {
 ┣⊱❥ *${prefix}report [lapor bug]*
 ┃͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏ ͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏.
 ┣━━━━°❀ ❬ *MEDIA* ❭ ❀°━━━━━┓
-┃
-┣⊱❥ *${prefix}stiker*
-┣⊱❥ *${prefix}tsticker*
-┣⊱❥ *${prefix}toimg*
-┣⊱❥ *${prefix}nulis*
-┣⊱❥ *${prefix}ocr*
-┣⊱❥ *${prefix}ytsearch*
-┣⊱❥ *${prefix}ytmp3*
-┣⊱❥ *${prefix}ytmp4*
-┣⊱❥ *${prefix}tiktok*
-┣⊱❥ *${prefix}tiktokstalk*
-┣⊱❥ *${prefix}fototiktok*
-┣⊱❥ *${prefix}igstalk*
-┣⊱❥ *${prefix}image*
-┣⊱❥ *${prefix}pinterest*
-┣⊱❥ *${prefix}tts*
-┣⊱❥ *${prefix}tes*
-┣⊱❥ *${prefix}tep*
-┣⊱❥ *${prefix}ttp*
-┣⊱❥ *${prefix}meme*
-┣⊱❥ *${prefix}memeindo*
-┣⊱❥ *${prefix}ssweb*
-┣⊱❥ *${prefix}walpaperhd*
-┣⊱❥ *${prefix}randomcat*
-┣⊱❥ *${prefix}joox*
-┣⊱❥ *${prefix}inu*
-┣⊱❥ *${prefix}elang*
-┣⊱❥ *${prefix}unta*
-┣⊱❥ *${prefix}anjing*
-┣⊱❥ *${prefix}babi*
-┣⊱❥ *${prefix}playstore*
-┣⊱❥ *${prefix}url2image*
-┣⊱❥ *${prefix}kbbi*
-┣⊱❥ *${prefix}imoji*
-┣⊱❥ *${prefix}wait*
+┣⊱❥ *${prefix}stiker* *CUCCES✅*
+┣⊱❥ *${prefix}tsticker* *FAILED❌*
+┣⊱❥ *${prefix}toimg* *FAILED✅*
+┣⊱❥ *${prefix}nulis* *SUCCES✅*
+┣⊱❥ *${prefix}ocr* *PERBAIKAN*
+┣⊱❥ *${prefix}ytsearch* *FAILED❌*
+┣⊱❥ *${prefix}ytmp3* *FAILED❌*
+┣⊱❥ *${prefix}ytmp4* *FAILED❌*
+┣⊱❥ *${prefix}tiktok* *FAILED❌*
+┣⊱❥ *${prefix}tiktokstalk* *FAILED❌*
+┣⊱❥ *${prefix}fototiktok* *FAILED❌*
+┣⊱❥ *${prefix}igstalk* *FAILED❌*
+┣⊱❥ *${prefix}image* *FAILED❌*
+┣⊱❥ *${prefix}pinterest* *FAILED❌*
+┣⊱❥ *${prefix}tts* *FAILED❌*
+┣⊱❥ *${prefix}tes* *SUCCES✅*
+┣⊱❥ *${prefix}tep* *FAILED❌*
+┣⊱❥ *${prefix}ttp* *PERBAIKAN*
+┣⊱❥ *${prefix}meme* *PERBAIKAN*
+┣⊱❥ *${prefix}memeindo* *PERBAIKAN*
+┣⊱❥ *${prefix}ssweb* *PERBAIKAN*
+┣⊱❥ *${prefix}walpaperhd* *PERBAIKAN*
+┣⊱❥ *${prefix}randomcat* *FAILED❌*
+┣⊱❥ *${prefix}joox* *PERBAIKAN*
+┣⊱❥ *${prefix}inu* *PERBAIKAN
+┣⊱❥ *${prefix}elang* *SUCCES✅*
+┣⊱❥ *${prefix}unta* *SUCCES✅*
+┣⊱❥ *${prefix}anjing* *SUCCES✅*
+┣⊱❥ *${prefix}babi* *SUCCES✅*
+┣⊱❥ *${prefix}playstore* *PERBAIKAN*
+┣⊱❥ *${prefix}url2image* *PERBAIKAN*
+┣⊱❥ *${prefix}kbbi* *PERBAIKAN*
+┣⊱❥ *${prefix}imoji* *PERBAIKAN*
+┣⊱❥ *${prefix}wait* *PERBAIKAN*
 ┃
 ┣━━━━°❀ ❬ *CREATOR* ❭ ❀°━━━━┓
 ┃
-┣⊱❥ *${prefix}thunder*
-┣⊱❥ *${prefix}tahta*
-┣⊱❥ *${prefix}glitch <teks|teks>*
-┣⊱❥ *${prefix}phlogo <teks|teks>*
-┣⊱❥ *${prefix}wolflogo <teks|teks>*
-┣⊱❥ *${prefix}wolflogo2 <teks|teks>*
-┣⊱❥ *${prefix}quotemaker <tx|wtrmk|tema>*
-┣⊱❥ *${prefix}galaxtext*
-┣⊱❥ *${prefix}textdark*
-┣⊱❥ *${prefix}textblue*
-┣⊱❥ *${prefix}lovemake*
-┣⊱❥ *${prefix}stiltext*
-┣⊱❥ *${prefix}ninjalogo*
-┣⊱❥ *${prefix}party*
-┣⊱❥ *${prefix}rtext*
-┣⊱❥ *${prefix}water*
-┣⊱❥ *${prefix}lionlogo <teks|teks>*
-┣⊱❥ *${prefix}textscreen*
-┣⊱❥ *${prefix}text3d*
-┣⊱❥ *${prefix}epep*
-┣⊱❥ *${prefix}marvelogo <teks|teks>*
-┣⊱❥ *${prefix}snow <teks|teks>*
-┣⊱❥ *${prefix}firetext*
+┣⊱❥ *${prefix}thunder*✅
+┣⊱❥ *${prefix}tahta*❌
+┣⊱❥ *${prefix}glitch <teks|teks>*✅
+┣⊱❥ *${prefix}phlogo <teks|teks>*✅
+┣⊱❥ *${prefix}wolflogo <teks|teks>*✅
+┣⊱❥ *${prefix}wolflogo2 <teks|teks>*✅
+┣⊱❥ *${prefix}quotemaker <tx|wtrmk|tema>*❌
+┣⊱❥ *${prefix}galaxtext*❌
+┣⊱❥ *${prefix}textdark*❌
+┣⊱❥ *${prefix}textblue*❌
+┣⊱❥ *${prefix}lovemake*❌
+┣⊱❥ *${prefix}stiltext*❌
+┣⊱❥ *${prefix}ninjalogo*❌
+┣⊱❥ *${prefix}party*❌
+┣⊱❥ *${prefix}rtext*❌
+┣⊱❥ *${prefix}water*👀
+┣⊱❥ *${prefix}lionlogo <teks|teks>*❌
+┣⊱❥ *${prefix}textscreen*👀
+┣⊱❥ *${prefix}text3d*👀
+┣⊱❥ *${prefix}epep*👀
+┣⊱❥ *${prefix}marvelogo <teks|teks>*👀
+┣⊱❥ *${prefix}snow <teks|teks>*👀
+┣⊱❥ *${prefix}firetext*👀
 ┃
 ┣━━━°❀ ❬ *FUN&GAMES* ❭ ❀°━━━┓
 ┃
-┣⊱❥ *${prefix}truth*
-┣⊱❥ *${prefix}dare*
-┣⊱❥ *${prefix}tebakgambar*
-┣⊱❥ *${prefix}family100*
-┣⊱❥ *${prefix}caklontong*
-┣⊱❥ *${prefix}game*
-┣⊱❥ *${prefix}primbonjodoh*
-┣⊱❥ *${prefix}ramaljadian*
-┣⊱❥ *${prefix}mlherolist*
-┣⊱❥ *${prefix}bucin*
-┣⊱❥ *${prefix}persengay*
-┣⊱❥ *${prefix}ramalhp <nomor>*
-┣⊱❥ *${prefix}ceckjodoh*
+┣⊱❥ *${prefix}truth*✅
+┣⊱❥ *${prefix}dare*✅
+┣⊱❥ *${prefix}tebakgambar*✅
+┣⊱❥ *${prefix}family100*❌
+┣⊱❥ *${prefix}caklontong*✅
+┣⊱❥ *${prefix}game*✅
+┣⊱❥ *${prefix}primbonjodoh*👀
+┣⊱❥ *${prefix}ramaljadian*✅
+┣⊱❥ *${prefix}mlherolist*👀
+┣⊱❥ *${prefix}bucin*✅
+┣⊱❥ *${prefix}persengay*👀
+┣⊱❥ *${prefix}ramalhp <nomor>*✅
+┣⊱❥ *${prefix}ceckjodoh*✅
 ┃
 ┣━━━━━°❀ ❬ *ANIME* ❭ ❀°━━━━━┓
 ┃
-┣⊱❥ *${prefix}openanime*
-┣⊱❥ *${prefix}Naruto*
-┣⊱❥ *${prefix}Minato*
-┣⊱❥ *${prefix}Boruto*
-┣⊱❥ *${prefix}Hinata*
-┣⊱❥ *${prefix}Sakura*
-┣⊱❥ *${prefix}Sasuke*
-┣⊱❥ *${prefix}kaneki*
-┣⊱❥ *${prefix}toukacan*
-┣⊱❥ *${prefix}rize*
-┣⊱❥ *${prefix}akira*
-┣⊱❥ *${prefix}itori*
-┣⊱❥ *${prefix}kurumi*
-┣⊱❥ *${prefix}miku*
-┣⊱❥ *${prefix}anime*
-┣⊱❥ *${prefix}nekonime*
-┣⊱❥ *${prefix}loli*
-┣⊱❥ *${prefix}loli2*
-┣⊱❥ *${prefix}waifu*
-┣⊱❥ *${prefix}waifu2*
-┣⊱❥ *${prefix}wibu*
-┣⊱❥ *${prefix}randomanime*
-┣⊱❥ *${prefix}pokemon*
-┣⊱❥ *${prefix}artinama*
+┣⊱❥ *${prefix}openanime*✅
+┣⊱❥ *${prefix}Naruto*✅
+┣⊱❥ *${prefix}Minato*✅
+┣⊱❥ *${prefix}Boruto*✅
+┣⊱❥ *${prefix}Hinata*✅
+┣⊱❥ *${prefix}Sakura*✅
+┣⊱❥ *${prefix}Sasuke*✅
+┣⊱❥ *${prefix}kaneki*✅
+┣⊱❥ *${prefix}toukacan*✅
+┣⊱❥ *${prefix}rize*✅
+┣⊱❥ *${prefix}akira*✅
+┣⊱❥ *${prefix}itori*✅
+┣⊱❥ *${prefix}kurumi*✅
+┣⊱❥ *${prefix}miku*✅
+┣⊱❥ *${prefix}anime*✅
+┣⊱❥ *${prefix}nekonime*✅
+┣⊱❥ *${prefix}loli*✅
+┣⊱❥ *${prefix}loli2*✅
+┣⊱❥ *${prefix}waifu*✅
+┣⊱❥ *${prefix}waifu2*✅
+┣⊱❥ *${prefix}wibu*✅
+┣⊱❥ *${prefix}randomanime*✅
+┣⊱❥ *${prefix}pokemon*✅
+┣⊱❥ *${prefix}artinama*✅
 ┃
 ┣━━°❀ ❬ *INFO&EDUKASI* ❭ ❀°━━━┓
 ┃
-┣⊱❥ *${prefix}infogc*
-┣⊱❥ *${prefix}infogempa*
-┣⊱❥ *${prefix}infogithub*
-┣⊱❥ *${prefix}infocuaca*
-┣⊱❥ *${prefix}infonomor*
-┣⊱❥ *${prefix}infomobil*
-┣⊱❥ *${prefix}infomotor*
-┣⊱❥ *${prefix}grupinfo*
-┣⊱❥ *${prefix}lirik*
-┣⊱❥ *${prefix}quotes*
-┣⊱❥ *${prefix}cerpen*
-┣⊱❥ *${prefix}chord*
-┣⊱❥ *${prefix}wiki*
-┣⊱❥ *${prefix}brainly*
-┣⊱❥ *${prefix}resepmasakan*
-┣⊱❥ *${prefix}map*
+┣⊱❥ *${prefix}infogc*✅
+┣⊱❥ *${prefix}infogempa*✅
+┣⊱❥ *${prefix}infogithub*👀
+┣⊱❥ *${prefix}infocuaca*👀
+┣⊱❥ *${prefix}infonomor*✅
+┣⊱❥ *${prefix}infomobil*👀
+┣⊱❥ *${prefix}infomotor*👀
+┣⊱❥ *${prefix}grupinfo*👀
+┣⊱❥ *${prefix}lirik*👀
+┣⊱❥ *${prefix}quotes*✅
+┣⊱❥ *${prefix}cerpen*✅
+┣⊱❥ *${prefix}chord*👀
+┣⊱❥ *${prefix}wiki*❌
+┣⊱❥ *${prefix}brainly*👀
+┣⊱❥ *${prefix}resepmasakan*✅
+┣⊱❥ *${prefix}map*👀
 ┃
 ┣━━━━━°❀ ❬ *GRUP* ❭ ❀°━━━━━━┓
 ┃
-┣⊱❥ *${prefix}add*
-┣⊱❥ *${prefix}kick*
-┣⊱❥ *${prefix}promote*
-┣⊱❥ *${prefix}demote*
-┣⊱❥ *${prefix}setname*
-┣⊱❥ *${prefix}setdesc*
-┣⊱❥ *${prefix}welcome*
-┣⊱❥ *${prefix}nsfw*
-┣⊱❥ *${prefix}simih*
-┣⊱❥ *${prefix}grup [buka/tutup]*
-┣⊱❥ *${prefix}tagme*
-┣⊱❥ *${prefix}hidetag*
-┣⊱❥ *${prefix}tagall*
-┣⊱❥ *${prefix}otagall*
-┣⊱❥ *${prefix}fitnah*
-┣⊱❥ *${prefix}infogc*
-┣⊱❥ *${prefix}grupinfo*
-┣⊱❥ *${prefix}linkgrup*
-┣⊱❥ *${prefix}listadmins*
-┣⊱❥ *${prefix}openanime*
-┣⊱❥ *${prefix}edotense*
-┣⊱❥ *${prefix}kudeta*
+┣⊱❥ *${prefix}add*✅
+┣⊱❥ *${prefix}kick*✅
+┣⊱❥ *${prefix}promote*✅
+┣⊱❥ *${prefix}demote*✅
+┣⊱❥ *${prefix}setname*✅
+┣⊱❥ *${prefix}setdesc*✅
+┣⊱❥ *${prefix}welcome*✅
+┣⊱❥ *${prefix}nsfw*✅
+┣⊱❥ *${prefix}simih*✅
+┣⊱❥ *${prefix}grup [buka/tutup]*✅
+┣⊱❥ *${prefix}tagme*✅
+┣⊱❥ *${prefix}hidetag*✅
+┣⊱❥ *${prefix}tagall*✅
+┣⊱❥ *${prefix}otagall*✅
+┣⊱❥ *${prefix}fitnah*✅
+┣⊱❥ *${prefix}infogc*✅
+┣⊱❥ *${prefix}grupinfo*✅
+┣⊱❥ *${prefix}linkgrup*✅
+┣⊱❥ *${prefix}listadmins*✅
+┣⊱❥ *${prefix}openanime*✅
+┣⊱❥ *${prefix}edotense*✅
+┣⊱❥ *${prefix}kudeta*✅
 ┃
 ┣━━━━━°❀ ❬ *NSFW* ❭ ❀°━━━━━━┓
 ┃
-┣⊱❥ *${prefix}nsfwloli*
-┣⊱❥ *${prefix}nsfwblowjob*
-┣⊱❥ *${prefix}nsfwneko*
-┣⊱❥ *${prefix}nsfwtrap*
-┣⊱❥ *${prefix}randomhentai*
-┣⊱❥ *${prefix}hentai*
-┣⊱❥ *${prefix}indohot*
+┣⊱❥ *${prefix}nsfwloli*❌
+┣⊱❥ *${prefix}nsfwblowjob*❌
+┣⊱❥ *${prefix}nsfwneko*❌
+┣⊱❥ *${prefix}nsfwtrap*❌
+┣⊱❥ *${prefix}randomhentai*❌
+┣⊱❥ *${prefix}hentai*❌
+┣⊱❥ *${prefix}indohot*❌
 ┃
 ┣━━━━°❀ ❬ *KERANG* ❭ ❀°━━━━━┓
 ┃
-┣⊱❥ *${prefix}apakah*
-┣⊱❥ *${prefix}kapankah*
-┣⊱❥ *${prefix}bisakah*
-┣⊱❥ *${prefix}rate*
-┣⊱❥ *${prefix}watak*
-┣⊱❥ *${prefix}hobby*
+┣⊱❥ *${prefix}apakah*✅
+┣⊱❥ *${prefix}kapankah*✅
+┣⊱❥ *${prefix}bisakah*✅
+┣⊱❥ *${prefix}rate*✅
+┣⊱❥ *${prefix}watak*✅
+┣⊱❥ *${prefix}hobby*✅
 ┃
 ┣━━━━━°❀ ❬ *OTHER* ❭ ❀°━━━━━┓
 ┃
-┣⊱❥ *${prefix}blocklist*
-┣⊱❥ *${prefix}testime*
-┣⊱❥ *${prefix}hilih*
-┣⊱❥ *${prefix}say*
-┣⊱❥ *${prefix}delete*
-┣⊱❥ *${prefix}shorturl*
+┣⊱❥ *${prefix}blocklist*✅
+┣⊱❥ *${prefix}testime*✅
+┣⊱❥ *${prefix}hilih*✅
+┣⊱❥ *${prefix}say*✅
+┣⊱❥ *${prefix}delete*✅
+┣⊱❥ *${prefix}shorturl*✅
 ┃
 ┣━━━━°❀ ❬ *OWNER* ❭ ❀°━━━━━┓
 ┃
-┣⊱❥ *${prefix}bc*
-┣⊱❥ *${prefix}ban*
-┣⊱❥ *${prefix}block*
-┣⊱❥ *${prefix}unblock*
-┣⊱❥ *${prefix}clearall*
-┣⊱❥ *${prefix}clone*
-┣⊱❥ *${prefix}getses*
-┣⊱❥ *${prefix}setpp*
-┣⊱❥ *${prefix}setpp*
-┣⊱❥ *${prefix}leave*
+┣⊱❥ *${prefix}bc*✅
+┣⊱❥ *${prefix}ban*✅
+┣⊱❥ *${prefix}block*✅
+┣⊱❥ *${prefix}unblock*✅
+┣⊱❥ *${prefix}clearall*✅
+┣⊱❥ *${prefix}clone*✅👀
+┣⊱❥ *${prefix}getses*👀
+┣⊱❥ *${prefix}setpp*❌
+┣⊱❥ *${prefix}setpp*✅
+┣⊱❥ *${prefix}leave*✅
 ┃
 ┣━━━━━°❀ ❬ *NAME* ❭ ❀°━━━━━┓
 ┃
@@ -225,23 +229,17 @@ const help = (prefix) => {
 ┃
 ┣━━━━°❀ *❬ NOTE ❭* ❀°━━━━━━⊱
 ┃
-┃Bot ini belum selesai sepenuhnya
-┃Masih dalam proses pengerjaan.
-┃
-┣━━━━━°❀ ❬ *TQTO* ❭ ❀°━━━━━┛
-┃
-┃ *SPECIAL THANGKS TO🤝*
-┣⊱❥ *${prefix}XPTN*
-┣⊱❥ *${prefix}FADHILGRAPHY*
-┣⊱❥ *${prefix}ALFA*
-┣⊱❥ *${prefix}ARUGAZ*
-┣⊱❥ *${prefix}DUINGZ*
-┣⊱❥ *${prefix}YUKINIKO*
-┣⊱❥ *${prefix}MHANKBARBAR*
-┣⊱❥ *${prefix}MYBOT TEAM*
-┣⊱❥ *AKMAL CAPY*
-┣⊱❥ *DLL*
-┃
+┃BOT INI MASIH DALAM PERBAIKAN!
+┃JADI TDK SEMUA FITUR BISA DI
+┃GUNAKAN!!
+┣━━━━°❀ *❬ APA LO ❭* ❀°━━━━━━⊱
+
+
+
+
+
+
+
 ┣━━━━━━━━━━━━━━━━━━━━┓
 ┃ ❬ *POWERED BY SAKURA-BOT* ❭
 ┗━━━━━━━━━━━━━━━━━━━━┛`
